c03b88b94380f29e3b0451aa990da9623fce9b7e
nascimento <nascimento.vinicius32@gmail.com>
2020-04-13 22:47:31 -0400

    list and upload file

--- start/routes.js
+++ start/routes.js
@@ -5,6 +5,10 @@ const Route = use('Route')
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
+
+Route.post('/files', 'FileController.store')
+Route.get('/files/:id', 'FileController.show')
 
 /*
 |--------------------------------------------------------------------------
